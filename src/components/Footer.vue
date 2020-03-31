e05c5605b86fcb5ac4c9438b8a6cc7564671edfa
원더 <wonderbotsupport@naver.com>
2020-03-31 12:04:52 +0900

    Fixed SomeTHings

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="ui divider" />
     <h4>
-      이 페이지는 <i class="icon red heart" />와 <i class="icon vuejs" />로
+      이 페이지는 <a v-on:click="add()" :href="count > 4 ? '/PHHI' : '#'" > <i  class="icon red heart" /></a>와 <i class="icon vuejs" />로
       제작되었습니다.<br />
       Copyright 2020 wonderlandpark, All rights reserved.<br />
-      <a href="https://github.com/wonderlandpark/phone-hub"
+      <a href="https://github.com/wonderlandpark/phonepare"
         ><i class="icon github" />Github</a
       >
     </h4>
@@ -14,6 +14,25 @@
 
 <script>
 export default {
-  name: "Footer"
+  name: "Footer",
+  methods: {
+    add: function(){
+      this.count++
+      if(this.count == 4) {
+        var con_test = confirm("Are you Sure?");
+        if(con_test == true){
+        this.count++
+        alert("PH Mode Enabled Press One More to Active.")
+        }
+
+      }
+      else if (this.count > 5) this.count = 0
+    }
+  },
+  data(){
+    return {
+      count: 0
+    }
+  }
 };
 </script>
