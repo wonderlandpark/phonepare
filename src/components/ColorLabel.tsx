9ae23c0668203e9167ef83cc73ce817bd4089da4
wonderlandpark <wonderlandpark@outlook.kr>
2021-08-26 21:51:28 +0900

    feat(ColorLabel): support for mobile

--- src/components/ColorLabel.tsx
+++ src/components/ColorLabel.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react'
 import type { Color } from '@/utils/types'
 import Label from './Label'
+import { Text } from '@chakra-ui/react'
 
 const ColorLabel: FC<{ color: Color }> = ({ color }) => {
-  return <Label bg={color.color.background || 'gray.100'} color={color.color.color || 'black'} font>
-    {color.name}
+  return <Label bg={color.color.background || 'gray.100'} color={color.color.color || 'black'} fontSize='sm'>
+    <Text display={{ base: 'none', md: 'block'}}>{color.name}</Text>
   </Label>
 }
 
