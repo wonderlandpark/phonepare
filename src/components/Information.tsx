39cc04d496940db2e781d9a3ced77a2b0111f696
wonderlandpark <wonderlandpark@outlook.kr>
2021-08-30 22:44:52 +0900

    chore: changed data fontSize

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactChild } from 'react'
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading, Text } from '@chakra-ui/react'
 import type { IconType } from 'react-icons/lib'
 
 const Information: FC<{ name: string, data: ReactChild, icon?: IconType }> = ({
@@ -10,7 +10,7 @@ const Information: FC<{ name: string, data: ReactChild, icon?: IconType }> = ({
       Icon && <Icon size={70} style={{ margin: 'auto' }} />
     }
     <Heading fontSize='xl' my={1}>{name}</Heading>
-    <p>{data}</p>
+    <Text fontSize='lg'>{data}</Text>
   </Box>
 }
 
