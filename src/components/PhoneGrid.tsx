7695ccd7dea6705c3399b70f2edfc0cace238e20
wonderlandpark <wonderlandpark@outlook.kr>
2021-08-30 22:24:16 +0900

    feat: added PhoneGrid component

--- src/components/PhoneGrid.tsx
+++ src/components/PhoneGrid.tsx
@@ -2,7 +2,7 @@ import { Grid } from '@chakra-ui/react'
 import React, { FC } from 'react'
 
 const PhoneGrid: FC = ({ children }) => {
-  return <Grid templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }} px={{ base: 10, md: 32 }} columnGap={10}>
+  return <Grid templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }} columnGap={10}>
     {children}
   </Grid>
 }
